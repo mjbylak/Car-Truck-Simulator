8da91fde7edeb10fc26f4730a7891d9dc2bc61a6
Mitchell <mjbylak@uark.edu>
2022-03-15 20:28:30 -0500

    truck

--- pthread_multiple_arguments.c
+++ pthread_multiple_arguments.c
@@ -721,6 +721,8 @@ void *vehicle_routine(pmstr_t *pmstrpara)
 
 void vehicle_arrival(pmstr_t *pmstrpara)
 {
+	pmstrpara->vehicle_type = 0;
+	
 	if(pmstrpara->vehicle_type)
 	{
 		if (pmstrpara->direction)
