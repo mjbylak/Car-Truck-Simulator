af44e381124ef732f34fe6c962635dfaaa1bd85f
Mitchell B <mitchell.bylak@gmail.com>
2022-03-16 18:58:26 -0500

    asdfasd

--- pthread_multiple_arguments.c
+++ pthread_multiple_arguments.c
@@ -633,10 +633,10 @@ void *vehicle_routine(void *pmstrpara_meth_arg)
 	//Checking to see if the car cannot cross, matching conditions like moving car at max of three
 	// or any moving trucks, or any waiting trucks
 	// or moving car in different direction
-	int cantCross = 	0;
-						/*(movingcar == 3 || movingtruck != 0) ||
+	int cantCross = 	(movingcar == 3 || movingtruck != 0) ||
 						(waitingtrucknorth != 0 || waitingtrucksouth != 0) ||
-						(movingcar > 0 && pmstrpara->direction != currentmovingdir);*/
+						(movingcar > 0 && pmstrpara->direction != currentmovingdir);
+						/*0*/
 		//while (this vehicle cannot cross) {
 		while (cantCross){
 			if(pmstrpara->direction == 0) 
