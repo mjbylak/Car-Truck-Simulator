4e31b72c362d770e34fa847562a67d16e590fd4f
Mitchell <mjbylak@uark.edu>
2022-03-15 19:03:37 -0500

    fix

--- pthread_multiple_arguments.c
+++ pthread_multiple_arguments.c
@@ -170,10 +170,12 @@ int main(void)
             direct = rand() % 2;
             args.vehicle_id = j;
             args.direction = direct;
-            if(r <= carprob){
+            /*if(r <= carprob){
                 args.vehicle_type = 1;
             }
-            else args.vehicle_type = 0;            
+            else args.vehicle_type = 0;   */
+			args.vehicle_type = 0;
+
 
             //call vehicle_arrival()
             vehicle_arrival(&args);
