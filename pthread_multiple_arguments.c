b0c2382653fdd3a742ed28fb9705c58a3b883158
Mitchell <mjbylak@uark.edu>
2022-03-16 00:35:46 -0500

    f

--- pthread_multiple_arguments.c
+++ pthread_multiple_arguments.c
@@ -726,13 +726,13 @@ void *vehicle_routine(void *pmstrpara_meth_arg)
 		}
 		if(movingcar > 0) {
 			if(waitingtrucknorth == 0 || waitingtrucksouth == 0){
-				if (waitingcarnorth > 0) {
+				if (currentmovingdir == 0) {
 					for(int i = movingcar; i < 3; i ++){
 						pthread_cond_signal(&CarNorthMovable);
 					} 
 					
 				}
-				else if (waitingcarsouth > 0) {
+				else if (currentmovingdir == 1) {
 					for(int i = movingcar; i < 3; i ++){
 						pthread_cond_signal(&CarSouthMovable);
 					} 
